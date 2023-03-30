7f86daae0cb87e56d7d5168b7487b7431df03238
ваше_имя <grigorenko-marko@rambler.ru>
2023-03-30 21:47:58 +0300

    add task03_1

--- task03/Program.cs
+++ task03/Program.cs
@@ -14,13 +14,14 @@ int y2 = Read("y2");
 
 double Magic(int x1, int y1, int x2, int y2)
 {
-    double X = x2 - x1;
-    if (X < 0) X = -X;
+    /*double X = x2 - x1;
     double Y = y2 - y1;
-    if (Y < 0) Y = -Y;
     double C1 = (double)Math.Pow(X, 2) + (double)Math.Pow(Y, 2);
     double C = (double)Math.Sqrt(C1);
-    return C;
+    return C;*/
+
+    return Math.Round(Math.Sqrt(Math.Pow(x1-x2,2)+Math.Pow(y1-y2,2)),3);
 }
+
 double C = Magic(x1, y1, x2, y2);
 System.Console.WriteLine($"Дина отрезка - {C}");
